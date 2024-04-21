3f949c49d00560deb99001df24bc5c93c3ddb67b
Stephan Strauss <StephanStrauss@gmx.ch>
2024-04-21 21:52:00 +0200

    Remove unnecessary comments

--- installer.sh
+++ installer.sh
@@ -2,29 +2,22 @@
 
 original_dir=$(pwd)
 
-# Erstelle temporäres Verzeichnis für die Installation
 mkdir -p /tmp/phonon-docker && cd /tmp/phonon-docker
 
-# Lade TensorFlow-Bibliotheken herunter
 curl -L https://storage.googleapis.com/tensorflow/versions/2.16.1/libtensorflow-gpu-linux-x86_64.tar.gz | tar -xz
 
-# Kopiere die notwendigen Dateien in das entsprechende Verzeichnis
 rm -rf "$original_dir/docker/templates/c/include" "$original_dir/docker/templates/c/lib"
 cp -r include lib "$original_dir/docker/templates/c/"
 rm -rf "$original_dir/docker/templates/cpp/include" "$original_dir/docker/templates/cpp/lib"
 cp -r include lib "$original_dir/docker/templates/cpp/"
 
-# Aufräumen
 rm -rf /tmp/phonon-docker
 
-# Baue das Docker-Image
 cd "$original_dir/docker"
 docker build -f ThePhononDockerFile -t tensor .
 
-# Zurück zum ursprünglichen Verzeichnis
 cd ..
 
-# Nachfragen, ob das Skript zur Ausführung hinzugefügt werden soll
 read -p "Do you want to install the running script to your .zshrc and .bashrc files at the beginning of these files? (y/n) " response
 
 if [[ $response =~ ^[Yy]$ ]]; then
@@ -35,7 +28,6 @@ if [[ $response =~ ^[Yy]$ ]]; then
     cp $file_source $target_dir/tensor_run.sh
     chmod +x $target_dir/tensor_run.sh
 
-    # Füge den Befehl zur Ausführung des Skripts den Shell-Startup-Dateien hinzu
     for file in .zshrc .bashrc; do
         file_path="$HOME/$file"
         if [ -f $file_path ]; then
